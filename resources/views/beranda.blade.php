efda2d04c0fc1b63e0d5a18bb535e9042a406e3c
antonroycar <antonnababan214@gmail.com>
2023-11-29 11:33:49 +0700

    update new beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,69 +5,104 @@
 @section('content')
     <style>
         .sensor-status-container {
-            display: flex;
-            justify-content: space-between;
-            flex-wrap: wrap;
-            /* Ini akan memungkinkan kartu untuk bungkus jika tidak cukup ruang */
-            margin-bottom: 20px;
-        }
+                display: flex;
+                justify-content: space-between;
+                flex-wrap: wrap;
+                /* Ini akan memungkinkan kartu untuk bungkus jika tidak cukup ruang */
+                margin-bottom: 20px;
+            }
 
-        .sensor-status-card {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            transition: 0.3s;
-            width: calc(50% - 10px);
-            /* Mengatur lebar untuk menjadi setengah dari container dikurangi margin */
-            border-radius: 5px;
-            padding: 16px;
-            background-color: #fff;
-            text-align: left;
-            margin-bottom: 20px;
-            border: 1px solid #ddd;
-            min-height: 100px;
-            /* Menetapkan ketinggian minimal */
-            display: flex;
-            /* Menambahkan flex untuk menyelaraskan judul dan isi */
-            flex-direction: column;
-            /* Menyusun judul dan isi secara vertikal */
-        }
+            @media (max-width: 768px) {
+                /* Adjust this value as needed for your responsive breakpoint */
+                .sensor-status-container {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    /* Center the cards */
+                }
 
-        .sensor-status-card h4 {
-            margin-bottom: auto;
-            /* Mendorong isi ke bawah jika tidak ada daftar item */
-            text-align: center;
-            /* Menyelaraskan teks ke tengah */
-        }
+                .sensor-status-card {
+                    max-width: 600px;
+                    /* Adjust this to match the charts' width */
+                    width: 100%;
+                    box-sizing: border-box;
+                    margin: 0 auto 20px;
+                    /* Center the card and add space below */
+                }
 
-        .sensor-status-list {
-            display: grid;
-            flex-direction: column;
-            justify-content: start;
-            grid-template-columns: repeat(2, 1fr);
-            /* Create two columns */
-            grid-gap: 8px;
-            /* Space between items */
-            list-style-type: none;
-            padding-left: 0;
-            margin-top: 0;
-        }
+                .chart-row {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    /* Center the charts */
+                }
 
-        .sensor-status-list li {
-            padding: 8px 16px;
-            background-color: #f2f2f2;
-            border-radius: 4px;
-            font-size: 14px;
-            color: #333;
-            text-align: center;
-        }
+                .chart {
+                    width: 100%;
+                    /* Full width for the chart */
+                    max-width: 600px;
+                    /* Match the max-width of the sensor-status-card */
+                    margin-bottom: 20px;
+                    /* Space between the charts */
+                }
+            }
 
-        .sensor-status-list li.dead {
-            background-color: #ffcdd2;
-        }
+            .sensor-status-card {
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                transition: 0.3s;
+                width: calc(50% - 10px);
+                /* Mengatur lebar untuk menjadi setengah dari container dikurangi margin */
+                border-radius: 5px;
+                padding: 16px;
+                background-color: #fff;
+                text-align: left;
+                margin-bottom: 20px;
+                border: 1px solid #ddd;
+                min-height: 100px;
+                /* Menetapkan ketinggian minimal */
+                display: flex;
+                /* Menambahkan flex untuk menyelaraskan judul dan isi */
+                flex-direction: column;
+                /* Menyusun judul dan isi secara vertikal */
+            }
 
-        .sensor-status-list li.alive {
-            background-color: #c8e6c9;
-        }
-    </style>
+            .sensor-status-card h4 {
+                margin-bottom: auto;
+                /* Mendorong isi ke bawah jika tidak ada daftar item */
+                text-align: center;
+                /* Menyelaraskan teks ke tengah */
+            }
+
+            .sensor-status-list {
+                display: grid;
+                flex-direction: column;
+                justify-content: start;
+                grid-template-columns: repeat(2, 1fr);
+                /* Create two columns */
+                grid-gap: 8px;
+                /* Space between items */
+                list-style-type: none;
+                padding-left: 0;
+                margin-top: 0;
+            }
+
+            .sensor-status-list li {
+                padding: 8px 16px;
+                background-color: #f2f2f2;
+                border-radius: 4px;
+                font-size: 14px;
+                color: #333;
+                text-align: center;
+            }
+
+            .sensor-status-list li.dead {
+                background-color: #ffcdd2;
+            }
+
+            .sensor-status-list li.alive {
+                background-color: #c8e6c9;
+            }
+        </style>
 
         <div class="sensor-status-container">
             <div class="sensor-status-card">
