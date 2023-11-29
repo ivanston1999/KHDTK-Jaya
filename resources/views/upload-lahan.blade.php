81f7edd9ca694b69a92b024d31270c18c1b73e99
ifs20024-itdel <89403503+ifs20024-itdel@users.noreply.github.com>
2023-11-29 12:12:14 +0700

    update

--- resources/views/upload-lahan.blade.php
+++ resources/views/upload-lahan.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <h1>Upload Foto Lahan</h1>
     <hr>
-    <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
         @csrf
         <div class="form-group">
             <label for="name">Nama Lahan</label>
@@ -31,30 +31,49 @@
     </form>
 </div>
 
+<script>
+    function validateForm() {
+        let isValid = true;
+        let fields = ['name', 'location', 'photo'];
+        let fieldNames = ['Nama Lahan', 'Lokasi Lahan', 'Foto Lahan'];
+    
+        for (let i = 0; i < fields.length; i++) {
+            let field = document.getElementById(fields[i]);
+            if (!field.value) {
+                alert(fieldNames[i] + " harus diisi.");
+                field.focus();
+                isValid = false;
+                break; 
+            }
+        }
+    
+        return isValid; 
+    }
+</script>
+
 <style>
-h1 {
-    font-size: 28px;
-}
+    h1 {
+        font-size: 28px;
+    }
 
-label {
-    font-size: 14px;
-    font-weight: bold;
-}
+    label {
+        font-size: 14px;
+        font-weight: bold;
+    }
 
-.custom-file-input {
-    border-radius: 5px;
-    border: 1px solid #ced4da;
-}
+    .custom-file-input {
+        border-radius: 5px;
+        border: 1px solid #ced4da;
+    }
 
-.custom-file-label {
-    border-radius: 5px;
-    background-color: white;
-    border: 1px solid #ced4da;
-}
+    .custom-file-label {
+        border-radius: 5px;
+        background-color: white;
+        border: 1px solid #ced4da;
+    }
 
-.input-group {
-    margin-bottom: 1rem;
-}
-
+    .input-group {
+        margin-bottom: 1rem;
+    }
 </style>
 @endsection
