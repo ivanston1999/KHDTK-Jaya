c7c76d30a94b96036de8201b358d31876275c516
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-11-30 09:16:18 +0700

    add user-management sidebar

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -16,14 +16,12 @@ class adminController extends Controller
     {
         $sensorStatus = [];
         $currentTime = now();
-
         for ($sensorId = 1; $sensorId <= 9; $sensorId++) {
             // Mengambil data terakhir dari setiap sensor.
             $tableName = 'sensor' . $sensorId;
             $lastDataPoint = DB::table($tableName)
                 ->latest('Tanggal')
                 ->first();
-
             if ($lastDataPoint) {
                 $lastDataTime = Carbon::parse($lastDataPoint->Tanggal);
                 // Menentukan status sensor berdasarkan waktu terakhir data diterima.
@@ -33,7 +31,6 @@ class adminController extends Controller
                 $sensorStatus[$tableName] = 'Tidak Aktif';
             }
         }
-
         return view('beranda', [
             'sensorStatus' => $sensorStatus,
         ]);
