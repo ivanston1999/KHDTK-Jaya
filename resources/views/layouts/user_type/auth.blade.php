afdd180f5550eac9870dfb122530a782d4915798
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-11-30 11:05:07 +0700

    add userview

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -61,6 +61,15 @@
                     @include('layouts.footers.auth.footer')
                 </div>
             </main>
+        @elseif (\Request::is('user-management/add'))  
+            @include('layouts.navbars.admin.sidebar')
+            <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
+                @include('layouts.navbars.auth.nav')
+                <div class="container-fluid py-4">
+                    @yield('content')
+                    @include('layouts.footers.auth.footer')
+                </div>
+            </main>
         @else
             @include('layouts.navbars.auth.sidebar')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
