8fa2af1fd0b4936caed7f2ee09887a91741bd311
ifs20027-itdel <ifs20027@students.del.ac.id>
2023-12-01 08:42:46 +0700

    update

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -141,6 +141,15 @@
                     @include('layouts.footers.auth.footer')
                 </div>
             </main>
+            @elseif (\Request::is('drones/create'))  
+            @include('layouts.navbars.admin.sidebar')
+            <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
+                @include('layouts.navbars.auth.nav')
+                <div class="container-fluid py-4">
+                    @yield('content')
+                    @include('layouts.footers.auth.footer')
+                </div>
+            </main>
         @elseif (\Request::is('user-management*'))  
             @include('layouts.navbars.admin.sidebar')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
