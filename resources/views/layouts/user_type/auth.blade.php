51bc1d902ddd7598ed12e6fd52b10f8b6d8dca25
ifs20027-itdel <ifs20027@students.del.ac.id>
2023-12-01 08:42:16 +0700

    update

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -123,6 +123,15 @@
                     @include('layouts.footers.auth.footer')
                 </div>
             </main>
+            @elseif (\Request::is('uploads/create'))  
+            @include('layouts.navbars.admin.sidebar')
+            <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
+                @include('layouts.navbars.auth.nav')
+                <div class="container-fluid py-4">
+                    @yield('content')
+                    @include('layouts.footers.auth.footer')
+                </div>
+            </main>
         @elseif (\Request::is('drones'))  
             @include('layouts.navbars.admin.sidebar')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg overflow-hidden">
