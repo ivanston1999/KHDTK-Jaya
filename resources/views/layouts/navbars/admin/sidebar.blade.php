73f38ac72f96ae450a79bc830c84ae4441ffbb3a
ifs20021-itdel <ifs20021@students.del.ac.id>
2024-01-23 15:32:56 +0700

    Update Kalkulator

--- resources/views/layouts/navbars/admin/sidebar.blade.php
+++ resources/views/layouts/navbars/admin/sidebar.blade.php
@@ -35,7 +35,7 @@
         </a>
       </li>
 
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link {{ (Request::is('kalkulator') || Request::is('kalkulators') || Request::is('kalkulators/create') ? 'active' : '') }}" href="{{ url('kalkulator') }}">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
@@ -43,14 +43,14 @@
               </svg>            </div>
             <span class="nav-link-text ms-1">Kalkulator</span>
         </a>
-      </li>
+      </li> -->
    </li>
          <li class="nav-item">
         <a class="nav-link {{ (Request::is('hasil-admin*') ? 'active' : '') }}" href="{{ url('hasil-admin') }}">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V136c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H552v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
           </div>
-          <span class="nav-link-text ms-1">Hasil Kalkulator User</span>
+          <span class="nav-link-text ms-1"> Kalkulator </span>
         </a>
       </li>
       <!-- <li class="nav-item mt-2">
